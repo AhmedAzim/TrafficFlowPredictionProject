22bc08ec5dd6df4490c7a834d3a4ada7493777d1
Ahmed Azim <ahmed.azim117@gmail.com>
2023-09-05 16:19:53 +1000

    initial commit

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ import warnings
 import numpy as np
 import pandas as pd
 from data.data import process_data
-from keras.models import load_model
-from keras.utils.vis_utils import plot_model
+from tensorflow.keras.models import load_model
+from tensorflow.keras.utils import plot_model
 import sklearn.metrics as metrics
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+
 warnings.filterwarnings("ignore")
 
 
@@ -97,8 +98,9 @@ def main():
     lstm = load_model('model/lstm.h5')
     gru = load_model('model/gru.h5')
     saes = load_model('model/saes.h5')
-    models = [lstm, gru, saes]
-    names = ['LSTM', 'GRU', 'SAEs']
+    my_model = load_model('model/my_model.h5')
+    models = [lstm, gru, saes, my_model]
+    names = ['LSTM', 'GRU', 'SAEs', 'My Model']
 
     lag = 12
     file1 = 'data/train.csv'
@@ -108,7 +110,7 @@ def main():
 
     y_preds = []
     for name, model in zip(names, models):
-        if name == 'SAEs':
+        if name == 'SAEs' or name == 'My Model':
             X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1]))
         else:
             X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
